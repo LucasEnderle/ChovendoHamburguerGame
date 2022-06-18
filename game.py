f3d426a01e4133fd57597b3d732b11df8a94f902
Lucas Tomasi Enderle <lucas.tmse@gmail.com>
2022-06-18 14:10:53 -0300

    Movimentos implementados

--- game.py
+++ game.py
@@ -21,10 +21,18 @@ def main():
     jogando = True
     movimentoX = random.randrange(0, largura)
     movimentoY = 0
+    velocidade = 1
+    direcao = True
+    posicaoXPlayer = 0
+    posicaoYPlayer = 373
+    movimentoXPlayer = 0
+    boneco = pygame.image.load("assets/personagem.png")
     badBurger = pygame.image.load("assets/badburger.png")
     pygame.mixer.music.load("assets/trilha.mp3")
     pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.1)
+    larguraPlayer = 101
+    velocidadePlayer = 10
 
     while True:
 
@@ -32,12 +40,36 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    movimentoXPlayer = -velocidadePlayer
+                elif event.key == pygame.K_RIGHT:
+                    movimentoXPlayer = velocidadePlayer
+                elif event.key == pygame.K_RETURN:
+                    main()
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    movimentoXPlayer = 0
+
 
         if jogando == True:
-            #aqui é a parte do jogo 
+            #aqui é a parte do jogo
+            posicaoXPlayer = posicaoXPlayer + movimentoXPlayer
+            if posicaoXPlayer < 0:
+                posicaoXPlayer = 0
+            elif posicaoXPlayer > largura - larguraPlayer:
+                posicaoXPlayer = largura - larguraPlayer
+            
 
             gameDisplay.blit(bg, (0,0))
+            gameDisplay.blit(boneco, (posicaoXPlayer, posicaoYPlayer))
             gameDisplay.blit(badBurger, (movimentoX, movimentoY))
+
+
+            if direcao == True:
+                if movimentoY <= 480 - 68:
+                    movimentoY = movimentoY + velocidade
   
         pygameDisplay.update()
         clock.tick(60)
