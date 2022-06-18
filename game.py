0663000d65c6271586028db27f1255e744802815
Lucas Tomasi Enderle <lucas.tmse@gmail.com>
2022-06-18 15:01:44 -0300

    Hamburguer caindo e voltando

--- game.py
+++ game.py
@@ -19,6 +19,8 @@ pygameDisplay.set_icon(icone)
 
 def main():
     jogando = True
+    rightPress = False
+    leftPress = False
     movimentoX = random.randrange(0, largura)
     movimentoY = 0
     velocidade = 1
@@ -42,19 +44,28 @@ def main():
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    movimentoXPlayer = -velocidadePlayer
+                    leftPress = True
                 elif event.key == pygame.K_RIGHT:
-                    movimentoXPlayer = velocidadePlayer
+                    rightPress = True
                 elif event.key == pygame.K_RETURN:
                     main()
 
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    movimentoXPlayer = 0
+                if event.key == pygame.K_LEFT:
+                    leftPress = False
+                elif event.key == pygame.K_RIGHT:
+                    rightPress = False
 
 
         if jogando == True:
             #aqui é a parte do jogo
+            movimentoXPlayer = 0
+            if rightPress:
+                movimentoXPlayer = velocidadePlayer
+            if leftPress:
+                movimentoXPlayer = -velocidadePlayer
+
+            #controlar limites do boneco
             posicaoXPlayer = posicaoXPlayer + movimentoXPlayer
             if posicaoXPlayer < 0:
                 posicaoXPlayer = 0
@@ -64,12 +75,19 @@ def main():
 
             gameDisplay.blit(bg, (0,0))
             gameDisplay.blit(boneco, (posicaoXPlayer, posicaoYPlayer))
-            gameDisplay.blit(badBurger, (movimentoX, movimentoY))
+            gameDisplay.blit(badBurger, (movimentoX - 68, movimentoY))
 
 
             if direcao == True:
                 if movimentoY <= 480 - 68:
                     movimentoY = movimentoY + velocidade
+                else:
+                    movimentoY = 0
+                    movimentoX = random.randrange(0, largura)
+                    velocidade = velocidade + 2
+                    direcao = True
+
+
   
         pygameDisplay.update()
         clock.tick(60)
